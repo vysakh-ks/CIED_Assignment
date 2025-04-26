2541b66bbfc0ddb7b5aefe8565ea2e84dd61ee9d
vysakh ks <80824539+vysakh-ks@users.noreply.github.com>
2025-04-26 11:51:36 +0530

    add changes

--- core/models.py
+++ core/models.py
@@ -20,14 +20,7 @@ class Medicines(models.Model):
     quantity = models.IntegerField(default=0)
     price = models.FloatField(default=0.0)
     date_added = models.DateTimeField(auto_now_add=True)
-    # packaging_price_multipier = models.IntegerField(default=1)
-    # PACKAGING_CHOICES = (
-    #     ('single', 'Single'),
-    #     ('strip', 'Strip'),
-    #     ('box', 'Box'),
-    #     ('pack', 'Pack'),
-    # )
-    # packaging_type = models.CharField(choices=PACKAGING_CHOICES, max_length=50)
+
     CATEGORIES_CHOICES = (
         ('tablet','Tablet'),
         ('capsule','Capsule'),
@@ -59,12 +52,3 @@ class Bills(models.Model):
         verbose_name_plural = 'Bills'
 
     
-# class SalesReports(models.Model):
-#     created_by = models.ForeignKey(MedicalShopUser, on_delete=models.CASCADE)
-#     medicine = models.ForeignKey(Medicines, on_delete=models.CASCADE)
-#     quantity = models.IntegerField()
-#     total_price = models.FloatField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     class Meta:
-#         verbose_name_plural = 'Sales Reports'
